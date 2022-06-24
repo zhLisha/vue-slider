c466dafa499414ffdfadf28074e53a9b737ab135
zhLisha <lisazhou1709@gmail.com>
2022-06-24 15:14:01 +0200

    BONUS 1 FOLDER

---
+++ BONUS 1/js/script.js
@@ -0,0 +1,54 @@
+var app = new Vue(
+    {
+        el: '#app',
+        data: {
+            currentSelectedElement: 0,
+
+            slides:[
+            {
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.',
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+            {
+                image: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+            }
+            ],    
+              
+        },
+        methods: {
+            nextThumb() {
+                if(this.currentSelectedElement < this.slides.length - 1) {
+                    this.currentSelectedElement++;
+                } else {
+                    this.currentSelectedElement = 0;
+                }
+            },
+
+            previousThumb() {
+                if(this.currentSelectedElement > 0) {
+                    this.currentSelectedElement--;
+                } else {
+                    this.currentSelectedElement = this.slides.length - 1;
+                }
+            }
+        }
+    }
+)
